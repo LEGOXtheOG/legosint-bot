a09637661c96554fb552dd9e4191d6e11ad4722c
LEGOXtheOG <legox.og@gmail.com>
2026-03-12 01:21:48 +0530

    Update legox.py

--- legox.py
+++ legox.py
@@ -1,6 +1,23 @@
 import telebot
 import requests
 import json
+
+from flask import Flask
+import threading
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is alive"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_web).start()
+
 import time
 from datetime import datetime,timedelta
 from telebot.types import InlineKeyboardMarkup,InlineKeyboardButton
